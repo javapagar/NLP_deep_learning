82eb4655a398bd02f04ceadf4b8af849c11ef025
javapagar <apariciogarcia.j@gmail.com>
2021-04-26 00:38:59 +0200

    new rap

--- src/scripts/giveMeFlow.py
+++ src/scripts/giveMeFlow.py
@@ -23,9 +23,18 @@ rap = ["rimando y en mi camino,",
 "que se han contrado en la cartera,",
 "esta es la cara de mi "]
 
+rap2 = ["a mi por lo que no te levanta el alma",
+        "y la fama de estar lo que me llevo a la tumba,",
+        "es la pera no te he contado", 
+        "no me llevo a la tumba,",
+        "es la espalda es mi sombra si disparan,",
+        "sobre el mil de antes",
+        "de la polla y la tumba,",
+        "la vida es"]
+
 
 for i in range(2):
-    for rima in rap:
+    for rima in rap2:
         saySomething(rima)
         delta = getLength()
         time.sleep(delta+0.5)
